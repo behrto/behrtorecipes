3e6efd035ef86f6a1b381748fbfec5552bb6770d
Behrendt <tom.behrendt@dm.de>
2021-05-06 17:01:33 +0200

    Edited ExtensionsAttribute Scripts

--- Templates/ExtensionAttribute-CFBundleVersion.sh
+++ Templates/ExtensionAttribute-CFBundleVersion.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 CFBundleVersion=""
 if [ -f "/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" ]; then
     CFBundleVersion=$(defaults read "/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" CFBundleVersion)
