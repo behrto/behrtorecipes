d3525688feb3395da0dae10c203531617809a2bf
Behrendt <tom.behrendt@dm.de>
2021-09-07 11:55:26 +0200

    Edited ExtensionAttribute Scipts with Userlocation

--- Templates/ExtensionAttribute-CFBundleShortVersion.sh
+++ Templates/ExtensionAttribute-CFBundleShortVersion.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+currentuser=`stat -f "%Su" /dev/console` #get currently logged in User
+
 CFBundleShortVersionString=""
 if [ -f "/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" ]; then
     CFBundleShortVersionString=$(defaults read "/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" CFBundleShortVersionString)
+elif [ -f "/Users/$currentuser/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" ]; then
+    CFBundleShortVersionString=$(defaults read "/Users/$currentuser/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" CFBundleShortVersionString)
 fi
 echo "<result>$CFBundleShortVersionString</result>"
 exit 0
--- Templates/ExtensionAttribute-CFBundleVersion.sh
+++ Templates/ExtensionAttribute-CFBundleVersion.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+currentuser=`stat -f "%Su" /dev/console` #get currently logged in User
+
 CFBundleVersion=""
 if [ -f "/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" ]; then
     CFBundleVersion=$(defaults read "/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" CFBundleVersion)
+elif [ -f "/Users/$currentuser/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" ]; then
+    CFBundleVersion=$(defaults read "/Users/$currentuser/Applications/%JSS_INVENTORY_NAME%/Contents/Info.plist" CFBundleVersion)
 fi
 echo "<result>$CFBundleVersion</result>"
 exit 0
